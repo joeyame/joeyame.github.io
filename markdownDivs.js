5a13795ffbad5f9d49d0872a7f14f7a2df7bdf0a
joeyame <81736071+joeyame@users.noreply.github.com>
2021-04-05 08:22:02 -0700

    Update markdownDivs.js

--- markdownDivs.js
+++ markdownDivs.js
@@ -1,6 +1,6 @@
-var scr = document.createElement("SCRIPT")
-scr.setAttribute('src', 'https://cdn.jsdelivr.net/npm/marked/marked.min.js');
-document.getElementsByTagName('head')[0].appendChild(scr);
+//var scr = document.createElement("SCRIPT")
+//scr.setAttribute('src', 'https://cdn.jsdelivr.net/npm/marked/marked.min.js');
+//document.getElementsByTagName('head')[0].appendChild(scr);
 
 var all = document.getElementsByTagName("div");
 
